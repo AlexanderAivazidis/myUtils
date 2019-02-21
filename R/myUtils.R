b1dd2c5051e1b6374ea4ca792ace41a365dcbd41
Alexander Aivazidis <aa16@internal.sanger.ac.uk>
2019-02-21 16:08:02 +0000

    update

--- R/myUtils.R
+++ R/myUtils.R
@@ -13,4 +13,25 @@
 cpm = function(data)
 {
   data = apply(data, 2, function(x) (x/sum(x))*10^6)
+  return(data)
 }
+
+#' Counts per million normalization
+#'
+#' This function converts an unnormalized countmatrix of genes x samples into read-per-kilobase normalization
+#' @param data The count matrix to be normalized
+#' @param geneLengths The gene lengths in the samer order as the rows of the count matrix
+#' @keywords normalization
+#' @export
+#' @examples
+#' rpkm()
+
+rpkm = function(data,geneLengths){
+  totalNumReads = colSums(data)
+  test = data / sapply(1:length(totalNumReads), function(x) geneLengths/10^3 * totalNumReads[x]/10^6)
+  return(data)
+}
+
+
+
+
